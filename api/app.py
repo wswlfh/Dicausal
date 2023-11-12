e031d4ec7b98115d7725d6130bdfc36e70cf227d
wswlfh <wswlfh@163.com>
2023-11-12 17:35:18 +0800

    update app.py

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from flask import Flask, request, jsonify
 import matplotlib
 import matplotlib.pyplot as plt
@@ -11,13 +13,17 @@ import shap
 import base64
 from io import BytesIO
 import random
+from copy import deepcopy
 from sklearn.ensemble import RandomForestRegressor
+from castle.algorithms import PC
+from castle.common.priori_knowledge import PrioriKnowledge
 
+font_path = pathlib.Path("simhei.ttf")
 matplotlib.use('agg')
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "*"}})  # 初始化 CORS
-CORS(app)  # 为 app 设置 CORS
 
+CORS(app)  # 为 app 设置 CORS
+# CORS(app, resources={r"/*": {"origins": "*"}})  # 初始化 CORS
 # CORS(app, origins=["http://localhost:3000"]) # 限制某个源才能访问
 
 df = None  # Initialize df as a global variable
@@ -64,7 +70,8 @@ def upload_file():
         for index, record in enumerate(dataSource):
             record['key'] = str(index)
 
-        return json.dumps({"columns": columns, "dataSource": dataSource}, ensure_ascii=False), 200, {'Content-Type': 'application/json;charset=utf-8'}
+        return json.dumps({"columns": columns, "dataSource": dataSource}, ensure_ascii=False), 200, {
+            'Content-Type': 'application/json;charset=utf-8'}
     else:
         return jsonify({"error": "No file uploaded"}),
 
@@ -78,7 +85,7 @@ def analyze_group(key_effects, other_effects, target_feature, model):
     shap_values = explainer(X)
 
     avg_shap_values = np.abs(shap_values.values).mean(axis=0)[
-        len(key_effects):]
+                      len(key_effects):]
     effect_shap_pairs = sorted(
         zip(other_effects, avg_shap_values), key=lambda x: x[1], reverse=True)
 
@@ -102,9 +109,10 @@ def analyze_group(key_effects, other_effects, target_feature, model):
         # Generate the summary plot for the current threshold
         selected_effect_names = [f[0] for f in selected_effects]
         filtered_shap_values = shap_values[:, [
-            X.columns.get_loc(f) for f in selected_effect_names]]
+                                                  X.columns.get_loc(f) for f in selected_effect_names]]
 
         buffer1 = BytesIO()
+        plt.yticks(fontproperties=font_path)
         shap.summary_plot(filtered_shap_values.values,
                           X[selected_effect_names], plot_type="bar", show=False)
         plt.savefig(buffer1, format="png")
@@ -115,6 +123,7 @@ def analyze_group(key_effects, other_effects, target_feature, model):
         shap_values_for_pie = list(selected_effects_dict.values())
         shap_labels_for_pie = list(selected_effects_dict.keys())
         buffer2 = BytesIO()
+        plt.yticks(fontproperties=font_path)
         shap.summary_plot(filtered_shap_values.values,
                           X[selected_effect_names], show=False)
         plt.savefig(buffer2, format="png")
@@ -123,13 +132,14 @@ def analyze_group(key_effects, other_effects, target_feature, model):
 
         # Create the BytesIO object for pie chart
         buffer_pie = BytesIO()
-        plt.figure()
-        plt.pie(shap_values_for_pie,
-                labels=shap_labels_for_pie, autopct='%1.1f%%')
-        plt.title(
-            f'{key_effects[0]}: SHAP Value Distribution for threshold {threshold}')
-        plt.savefig(buffer_pie, format="png")
-        plt.close()
+        # plt.figure()
+        # plt.yticks(fontproperties=font_path)
+        # plt.pie(shap_values_for_pie,
+        #         labels=shap_labels_for_pie, autopct='%1.1f%%')
+        # plt.title(
+        #     f'{key_effects[0]}: SHAP Value Distribution for threshold {threshold}')
+        # plt.savefig(buffer_pie, format="png")
+        # plt.close()
         shap_3 = base64.b64encode(buffer_pie.getvalue()).decode()
 
         thresholds_results[f'threshold_{threshold}'] = {
@@ -212,26 +222,114 @@ def shap_analysis_api():
         return jsonify({"error": str(e)}), 400
 
 
-def modify_adj_matrix(adj_matrix, num_new_edges):
+def get_reachable_matrix(adj_matrix, n_subSystem):
+    origin_matrix = adj_matrix + np.identity(n_subSystem)
+    old_matrix = deepcopy(origin_matrix)
+    while True:
+        new_matrix = np.matmul(old_matrix, origin_matrix)
+        for i in range(n_subSystem):
+            for j in range(n_subSystem):
+                if new_matrix[i][j] >= 1:
+                    new_matrix[i][j] = 1
+        if (old_matrix == new_matrix).all():
+            return new_matrix - np.identity(n_subSystem)
+        old_matrix = deepcopy(new_matrix)
+
+
+def get_subsystems(sub_adj_matrix, nodes):
+    subsystems = []
+    for i in range(len(sub_adj_matrix)):
+        subsystems.append({'key_id': [], 'id': []})
+
+    for n in nodes:
+        try:
+            n_id = int(n['id'])
+            sub_id = int(n['comboId'][-1])
+            if n['type'] == 'effect':
+                subsystems[sub_id]['id'].append(n_id)
+            if n['type'] == 'key':
+                subsystems[sub_id]['key_id'].append(n_id)
+        except:
+            subsystems[sub_id+1]['key_id'].append(n_id)
+
+    return subsystems
+
+
+def get_dataset(nodes):
+    lables = []
+    for n in nodes:
+        lables.append(n['label'])
+    X = df[lables]
+    return X
+
+
+def get_priori_knowledge(adj_matrix, nodes, sub_adj_matrix):
+    # 初始化可达矩阵
+    reach_matrix = get_reachable_matrix(sub_adj_matrix, len(sub_adj_matrix))
+    # 初始化subsystems
+    subsystems = get_subsystems(sub_adj_matrix, nodes)
+    # 创建先验知识实例
+    pk = PrioriKnowledge(len(nodes))
+
+    # 构造先验知识
+    # 加入required edges
+    for i in range(len(nodes)):
+        for j in range(len(nodes)):
+            if adj_matrix[i][j] == 1:
+                pk.add_required_edge(i, j)
+                pk.add_forbidden_edge(j, i)
+
+    # 组内：key不能指向同组的effect
+    for i in range(len(subsystems)):
+        item = subsystems[i]
+        for i in item['id']:
+            for j in item['key_id']:
+                pk.add_forbidden_edge(j, i)
+
+    # 组间：下游不能指向上游
+    for up in range(len(reach_matrix)):
+        for down in range(len(reach_matrix)):
+            if reach_matrix[up][down] == 0:
+                continue
+            for x_down in subsystems[down]['id']:
+                for x_up in subsystems[up]['id']:
+                    # 1.1 Forbidden x->x
+                    pk.add_forbidden_edge(x_down, x_up)
+                for y_up in subsystems[up]['key_id']:
+                    # 1.2 Forbidden x->y
+                    pk.add_forbidden_edge(x_down, y_up)
+
+            for y_down in subsystems[down]['key_id']:
+                for x_up in subsystems[up]['id']:
+                    # 2.1 Forbidden y->x
+                    pk.add_forbidden_edge(y_down, x_up)
+                for y_up in subsystems[up]['key_id']:
+                    # 2.2 Forbidden y->y
+                    pk.add_forbidden_edge(y_down, y_up)
+    return pk
+
+
+def get_causal_matrix(X, priori_knowledge=None):
+    pc = PC(variant='original', priori_knowledge=priori_knowledge)
+    pc.learn(X)
+    causal_matrix = pc.causal_matrix
+    causal_matrix_ls = []
+    for row in range(causal_matrix.shape[0]):
+        causal_matrix_ls.append([])
+        for col in range(causal_matrix.shape[1]):
+            causal_matrix_ls[row].append(int(causal_matrix[row][col]))
+    return causal_matrix_ls
+
+
+def modify_adj_matrix(adj_matrix, sub_adj_matrix, nodes):
     # 获取矩阵的维度
-    rows, cols = len(adj_matrix), len(adj_matrix[0])
+    assert len(adj_matrix) == len(adj_matrix[0])
+    assert len(sub_adj_matrix) == len(sub_adj_matrix[0])
 
-    added_edges = 0
-    while added_edges < num_new_edges:
-        row = random.randint(0, rows - 2)  # -2是因为要保持最后一行为全0
-        col = random.randint(0, cols - 1)
-
-        # 确保不添加自环
-        if row == col:
-            continue
-
-        # 确保不添加已经存在的边
-        if adj_matrix[row][col] == 1:
-            continue
-
-        # 在邻接矩阵中将该单元格设置为1（表示有边）
-        adj_matrix[row][col] = 1
-        added_edges += 1
+    # 切片待计算的数据集
+    X = get_dataset(nodes)
+    pk = get_priori_knowledge(adj_matrix, nodes, sub_adj_matrix)
+    adj_matrix = get_causal_matrix(X, pk)
 
     return adj_matrix
 
@@ -244,19 +342,22 @@ def gen_causal_diagrams():
 
         # 节点邻接矩阵
         adj_matrix = json_data.get('adjMatrix', [])
-
         # 子系统邻接矩阵
+
         sub_adj_matrix = json_data.get('subAdjMatrix', [])
 
+        # 节点的属性，名称，id，类型，子系统归属
+        nodes = json_data.get('nodes', [])
+
+        print(nodes)
         print("接收到的节点邻接矩阵:", adj_matrix)
         print("接收到的子系统邻接矩阵:", sub_adj_matrix)
         # 从请求中获取需要添加的新边的数量，如果没有则默认为5
-        num_new_edges = 10
+        num_new_edges = 5
 
         # 修改邻接矩阵
-        modified_adj_matrix = modify_adj_matrix(adj_matrix, num_new_edges)
+        modified_adj_matrix = modify_adj_matrix(adj_matrix, sub_adj_matrix, nodes)
 
-        
         print("修改后的节点邻接矩阵:", modified_adj_matrix)
 
         return jsonify(modified_adj_matrix), 200
@@ -267,4 +368,4 @@ def gen_causal_diagrams():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
